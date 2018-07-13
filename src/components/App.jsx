bcbeba1b33233a88d2bf2cbef35bcf83813031bb
Unknown <victor.graffard@gmail.com>
2018-07-13 16:30:02 +0200

    React sample uploaded

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import FunctionalComponent from './FunctionalComponent';
+import ClassComponent from './ClassComponent';
+import '../../style/style.css';
+
+
+class App extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            item: '',
+            items: []
+        };
+    }
+
+    addItem () {
+        if (!this.state.item) {
+            console.log('No item value.');
+        } else {
+            this.setState(() => {
+                items: this.state.items.push(this.state.item);
+            });
+        }
+    }
+
+    render () {
+        return (
+        <div className="app-container">
+            <input value={this.state.item} onChange={(e) => this.setState({ item: e.target.value })} />
+            <button onClick={() => this.addItem()} >Submit item</button>
+
+            <FunctionalComponent />
+            <ClassComponent />
+        </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/FunctionalComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../style/style.css';
+
+const FunctionalComponent = () => {
+    return (
+        <div className="functional-component-container">
+            FunctionalComponent return value.
+        </div>
+    );
+};
+
+export default FunctionalComponent;
